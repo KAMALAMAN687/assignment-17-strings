d4b9246df169a9cca914fffb74e7327239078c74
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-10-01 19:57:01 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[10];
+    int i;
+    fgets(str,10,stdin);
+    for(i=0;str[i];i++)
+    {
+
+    }
+    printf("%d",i-1);
+
+    
+
+}
